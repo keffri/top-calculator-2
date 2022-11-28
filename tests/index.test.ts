0744dac19d2f0a72ff6285deb63791c6a80fc030
keffri <keffri.w.neal@gmail.com>
2022-11-28 13:29:46 -0800

    operate function tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -17,11 +17,37 @@ describe('performs correct operation', () => {
     expect(Calculator.multiply(13, 6)).toBe(78);
   });
 
-  test('divides 14 / 3 to equal 4.67', () => {
+  test('divides 14 / 3 to equal 4.66', () => {
     expect(Calculator.divide(14, 3)).toBe(4.66);
   });
 
-  test('divides 63 / 14.5 to equal 4.67', () => {
+  test('divides 63 / 14.5 to equal 4.34', () => {
     expect(Calculator.divide(63, 14.5)).toBe(4.34);
+  });
+});
+
+describe('use selected operator to perform correct operation', () => {
+  test('uses add operator to add 1 + 2 to equal 3', () => {
+    expect(Calculator.operate('add', '1', '2')).toBe(3);
+  });
+
+  test('uses subtract operator to subtract 7 - 2 to equal 5', () => {
+    expect(Calculator.operate('subtract', '7', '2')).toBe(5);
+  });
+
+  test('uses subtract operator to subtract 4 - -5 to equal 9', () => {
+    expect(Calculator.operate('subtract', '4', '-5')).toBe(9);
+  });
+
+  test('uses multiply operator to multiply 13 * 6 to equal 78', () => {
+    expect(Calculator.operate('multiply', '13', '6')).toBe(78);
+  });
+
+  test('uses divide operator to divide 14 / 3 to equal 4.66', () => {
+    expect(Calculator.operate('divide', '14', '3')).toBe(4.66);
+  });
+
+  test('uses divide operator to divide 63 / 14.5 to equal 4.34', () => {
+    expect(Calculator.operate('divide', '63', '14.5')).toBe(4.34);
   });
 });
