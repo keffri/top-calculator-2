53169f0c95781396f4ff1858c3376aa6cd4cab36
keffri <keffri.w.neal@gmail.com>
2022-11-28 13:47:34 -0800

    integrated old plain .js file in to .ts file to be updated

--- index.js
+++ index.js
@@ -30,4 +30,90 @@ var Calculator = {
         }
     }
 };
+// *** OLD  CODE *** //
+var display = document.getElementById('display');
+var numbers = document.querySelectorAll('.number');
+numbers.forEach(function (number) {
+    number.addEventListener('click', function (e) {
+        var numberValue = e.target.textContent;
+        console.log(numberValue);
+        updateDisplay(numberValue);
+    });
+});
+function updateDisplay(number) {
+    if (number === '.' && display.textContent.includes('.')) {
+        return;
+    }
+    if (display.textContent === '0') {
+        if (number === '0' || number === '.') {
+            return;
+        }
+        display.textContent = '';
+    }
+    else if (operation && previousValue) {
+        currentValue += number;
+        display.textContent = currentValue;
+        return;
+    }
+    previousValue += number;
+    display.textContent = previousValue;
+}
+var previousValue = '';
+var currentValue = '';
+function storeValue() {
+    if (!previousValue) {
+        previousValue = display.textContent;
+    }
+}
+var operation = null;
+var operators = document.querySelectorAll('.operator');
+operators.forEach(function (operator) {
+    operator.addEventListener('click', function (e) {
+        if (previousValue === '') {
+            return;
+        }
+        storeValue();
+        if (previousValue && currentValue) {
+            calculate();
+            operation = e.target.id;
+        }
+        else {
+            operation = e.target.id;
+        }
+    });
+});
+var answer;
+var equalsButton = document.getElementById('equals');
+equalsButton.addEventListener('click', function () {
+    calculate();
+});
+function calculate() {
+    if (!previousValue || !currentValue) {
+        return;
+    }
+    answer = Calculator.operate(operation, previousValue, currentValue);
+    previousValue = answer;
+    currentValue = '';
+    display.textContent = answer;
+}
+var deleteButton = document.getElementById('delete');
+deleteButton.addEventListener('click', deleteNumber);
+function deleteNumber() {
+    if (!currentValue) {
+        previousValue = String(previousValue).slice(0, -1);
+        display.textContent = display.textContent.slice(0, -1);
+    }
+    else {
+        currentValue = currentValue.slice(0, -1);
+        display.textContent = display.textContent.slice(0, -1);
+    }
+}
+var clearButton = document.getElementById('clear');
+clearButton.addEventListener('click', allClear);
+function allClear() {
+    previousValue = '';
+    currentValue = '';
+    operation = null;
+    display.textContent = '0';
+}
 exports["default"] = Calculator;
--- index.ts
+++ index.ts
@@ -36,4 +36,104 @@ const Calculator = {
   },
 };
 
+// *** OLD  CODE TO BE UPDATED *** //
+
+const display = document.getElementById('display');
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((number) => {
+  number.addEventListener('click', (e) => {
+    let numberValue = (e.target as HTMLElement).textContent;
+    console.log(numberValue);
+    updateDisplay(numberValue);
+  });
+});
+
+function updateDisplay(number) {
+  if (number === '.' && display.textContent.includes('.')) {
+    return;
+  }
+  if (display.textContent === '0') {
+    if (number === '0' || number === '.') {
+      return;
+    }
+    display.textContent = '';
+  } else if (operation && previousValue) {
+    currentValue += number;
+    display.textContent = currentValue;
+    return;
+  }
+  previousValue += number;
+  display.textContent = previousValue;
+}
+
+let previousValue = '';
+let currentValue = '';
+
+function storeValue() {
+  if (!previousValue) {
+    previousValue = display.textContent;
+  }
+}
+
+let operation = null;
+
+const operators = document.querySelectorAll('.operator');
+operators.forEach((operator) => {
+  operator.addEventListener('click', (e) => {
+    if (previousValue === '') {
+      return;
+    }
+    storeValue();
+    if (previousValue && currentValue) {
+      calculate();
+      operation = (e.target as HTMLElement).id;
+    } else {
+      operation = (e.target as HTMLElement).id;
+    }
+  });
+});
+
+let answer;
+
+const equalsButton = document.getElementById('equals');
+equalsButton.addEventListener('click', () => {
+  calculate();
+});
+
+function calculate() {
+  if (!previousValue || !currentValue) {
+    return;
+  }
+  answer = Calculator.operate(operation, previousValue, currentValue);
+  previousValue = answer;
+  currentValue = '';
+  display.textContent = answer;
+}
+
+const deleteButton = document.getElementById('delete');
+
+deleteButton.addEventListener('click', deleteNumber);
+
+function deleteNumber() {
+  if (!currentValue) {
+    previousValue = String(previousValue).slice(0, -1);
+    display.textContent = display.textContent.slice(0, -1);
+  } else {
+    currentValue = currentValue.slice(0, -1);
+    display.textContent = display.textContent.slice(0, -1);
+  }
+}
+
+const clearButton = document.getElementById('clear');
+
+clearButton.addEventListener('click', allClear);
+
+function allClear() {
+  previousValue = '';
+  currentValue = '';
+  operation = null;
+  display.textContent = '0';
+}
+
 export default Calculator;
