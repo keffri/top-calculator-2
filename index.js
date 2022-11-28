0c5ab673f59f488779c96ddf031fe96ccfbcd595
keffri <keffri.w.neal@gmail.com>
2022-11-28 13:30:00 -0800

    operate function added

--- index.js
+++ index.js
@@ -14,6 +14,20 @@ var Calculator = {
         var division = operandOne / operandTwo;
         var rounded = Math.floor(division * 100) / 100;
         return rounded;
+    },
+    operate: function (operator, operandOne, operandTwo) {
+        if (operator === 'add') {
+            return Calculator.add(parseFloat(operandOne), parseFloat(operandTwo));
+        }
+        else if (operator === 'subtract') {
+            return Calculator.subtract(parseFloat(operandOne), parseFloat(operandTwo));
+        }
+        else if (operator === 'multiply') {
+            return Calculator.multiply(parseFloat(operandOne), parseFloat(operandTwo));
+        }
+        else if (operator === 'divide') {
+            return Calculator.divide(parseFloat(operandOne), parseFloat(operandTwo));
+        }
     }
 };
 exports["default"] = Calculator;
--- index.ts
+++ index.ts
@@ -16,6 +16,24 @@ const Calculator = {
     let rounded = Math.floor(division * 100) / 100;
     return rounded;
   },
+
+  operate: function (operator: string, operandOne: string, operandTwo: string) {
+    if (operator === 'add') {
+      return Calculator.add(parseFloat(operandOne), parseFloat(operandTwo));
+    } else if (operator === 'subtract') {
+      return Calculator.subtract(
+        parseFloat(operandOne),
+        parseFloat(operandTwo)
+      );
+    } else if (operator === 'multiply') {
+      return Calculator.multiply(
+        parseFloat(operandOne),
+        parseFloat(operandTwo)
+      );
+    } else if (operator === 'divide') {
+      return Calculator.divide(parseFloat(operandOne), parseFloat(operandTwo));
+    }
+  },
 };
 
 export default Calculator;
